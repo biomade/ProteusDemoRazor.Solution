2cb417d1aa00104574436ec71039fba6691c2eb6
laurie kern <laurie.kern.ctr@us.af.mil>
2020-06-24 09:33:21 -0500

    put jquery script for menu in js file and add file to _Layout page. Setup custom layout for StyleGuild Build Widgets page

--- src/ProteusDemoRazor.UI/wwwroot/js/site.js
+++ src/ProteusDemoRazor.UI/wwwroot/js/site.js
@@ -1,4 +1,23 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+// Write your jQuery/Javascript code.
+
+
+$(document).ready(function () {
+    //this sets the active link , 
+    //the selected route/page will be selected
+
+    var url = window.location;
+
+    // for sidebar menu entirely but not cover treeview
+    $('ul.nav-sidebar a').filter(function () {
+        return this.href == url;
+    }).addClass('active');
+
+    // for treeview
+    $('ul.nav-treeview a').filter(function () {
+        return this.href == url;
+    }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
+
+});
