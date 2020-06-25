5f79ea06ed01bf52c0912eff8a05e9796d7b0e0d
laurie kern <laurie.kern.ctr@us.af.mil>
2020-06-25 11:34:38 -0500

    add pages (empty) for Mailbox section

---
+++ src/ProteusDemoRazor.UI/Areas/StyleGuide/Pages/Mailbox/Compose.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proteus.UI.Areas.StyleGuide.Pages.Mailbox
+{
+    public class ComposeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
