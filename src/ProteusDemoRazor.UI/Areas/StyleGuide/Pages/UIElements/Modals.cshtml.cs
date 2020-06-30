e9dfd4f3f6bef27551b740572e05abed9d4d72b2
laurie kern <laurie.kern.ctr@us.af.mil>
2020-06-30 10:36:10 -0500

    Finish UI pages

---
+++ src/ProteusDemoRazor.UI/Areas/StyleGuide/Pages/UIElements/Modals.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proteus.UI.Areas.StyleGuide.Pages.UIElements
+{
+    public class ModalsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
