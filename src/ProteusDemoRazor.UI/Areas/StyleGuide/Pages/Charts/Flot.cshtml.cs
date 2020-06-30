59f244d8be12d55c752bed13966f6fae4ca3101c
laurie kern <laurie.kern.ctr@us.af.mil>
2020-06-30 11:40:49 -0500

    Charts completed

---
+++ src/ProteusDemoRazor.UI/Areas/StyleGuide/Pages/Charts/Flot.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proteus.UI.Areas.StyleGuide.Pages.Charts
+{
+    public class FlotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
