de845d500414a8387401301ed389b501c052b0a9
laurie kern <laurie.kern.ctr@us.af.mil>
2020-07-21 15:14:02 -0500

    Delete User Role

--- src/ProteusDemoRazor.UI/Areas/Identity/Pages/UserRoles/Delete.cshtml.cs
+++ src/ProteusDemoRazor.UI/Areas/Identity/Pages/UserRoles/Delete.cshtml.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Proteus.Core.Entities.Identity;
 using Proteus.Infrastructure.Identity;
+using SmartBreadcrumbs.Attributes;
 
 namespace Proteus.UI.Areas.Identity.Pages.UserRoles
 {
+    [Breadcrumb("Delete", FromPage = typeof(IndexModel))]
+    [Authorize(Roles = "Administrator")]
     public class DeleteModel : PageModel
     {
         private readonly Proteus.Infrastructure.Identity.IdentityDbContext _context;
